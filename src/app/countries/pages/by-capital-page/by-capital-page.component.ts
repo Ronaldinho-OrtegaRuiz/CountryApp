0efad3db7bbb3b773fbc85675a5c13c8e691b4cc
Ronaldinho Ortega Ruiz <112998261+Ronaldinho-OrtegaRuiz@users.noreply.github.com>
2023-12-28 10:58:17 -0500

    Mostrar países en una tabla

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
 import {Component, Output} from '@angular/core';
+import {CountriesService} from "../../services/countries.service";
+import {Country} from "../../interfaces/country";
 
 @Component({
   selector: 'countries-by-capital-page',
@@ -7,8 +9,15 @@ import {Component, Output} from '@angular/core';
 })
 export class ByCapitalPageComponent {
 
+  public countries:Country[]=[];
+
+  constructor(private countriesService: CountriesService) {
+  }
+
   searchByCapital(term: string) {
-    console.log('searchByCapital');
-    console.log(term);
+    this.countriesService.searchCountryByCapital(term)
+      .subscribe(countries => {
+        this.countries = countries;
+      });
   }
 }
